9ab7443f0f00ef7bcc5f17545bd60ddc2c13d2ec
KartikeyAgarwal <66178523+KartikeyAgarwal@users.noreply.github.com>
2020-07-31 18:04:48 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
         
         <p className="copyright">
-         Built with <a href="http://gatsby-dimension.surge.sh">Gatsby.js</a> and ❤️ by <a href="http://www.adityaketkar.me"> Aditya Ketkar</a>  
+         Built with <a href="http://gatsby-dimension.surge.sh">Gatsby.js</a> and ❤️ by <a href="https://www.linkedin.com/in/kumar-kartikey-agarwal-6a71151a1/"> Kumar Kartikey Agarwal</a>  
         <br/>
         Get your own version of this webpage <a href="https://github.com/adityaketkar/circle-packing-personal-homepage"> here </a>
         </p>
