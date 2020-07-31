4a73c101c8e48254788436ccce302e7638ffb1dd
KartikeyAgarwal <66178523+KartikeyAgarwal@users.noreply.github.com>
2020-07-31 18:05:19 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = (props) => (
         <p className="copyright">
          Built with <a href="http://gatsby-dimension.surge.sh">Gatsby.js</a> and ❤️ by <a href="https://www.linkedin.com/in/kumar-kartikey-agarwal-6a71151a1/"> Kumar Kartikey Agarwal</a>  
         <br/>
-        Get your own version of this webpage <a href="https://github.com/adityaketkar/circle-packing-personal-homepage"> here </a>
+        Get your own version of this webpage <a href="https://github.com/KartikeyAgarwal/KartikeyAgarwal-personal-website"> here </a>
         </p>
     </footer>
 )
